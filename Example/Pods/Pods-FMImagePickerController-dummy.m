e32926179cccb8358e4e0a9bade059c657a29af0
Kyle Shank <kyle.shank@gmail.com>
2014-09-12 09:34:37 -0400

    remove pods

--- Example/Pods/Pods-FMImagePickerController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FMImagePickerController : NSObject
-@end
-@implementation PodsDummy_Pods_FMImagePickerController
-@end
--- Example/Pods/Pods-Tests-FMImagePickerController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Tests_FMImagePickerController : NSObject
-@end
-@implementation PodsDummy_Pods_Tests_FMImagePickerController
-@end
